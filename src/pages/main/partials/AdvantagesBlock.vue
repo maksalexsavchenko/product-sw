7c16bf9281343a6c57ccc9fd7e058aaaf54b15b2
maksalexsavchenko <makskidexom@gmail.com>
2022-09-29 14:50:42 +0300

    fix advantages block

--- src/pages/main/partials/AdvantagesBlock.vue
+++ src/pages/main/partials/AdvantagesBlock.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="advantages-block container">
     <div
-      v-for="(item, index) in advantages"
-      :key="index"
-      class="advantages-block--item"
+        v-for="(item, index) in advantages"
+        :key="index"
+        class="advantages-block--item"
     >
-      <img :src="item.img" alt="" />
-      <h1>{{ item.title }}</h1>
-      <p>{{ item.text }}</p>
+      <div class="advantages-block--item_info">
+        <div class="advantages-block--item_info-title">
+          {{ item.title }}
+        </div>
+        <div class="advantages-block--item_info-text">
+          {{ item.text }}
+        </div>
+      </div>
+      <div class="advantages-block--item_img">
+        <img :src="item.img" alt=""/>
+      </div>
     </div>
   </div>
 </template>
@@ -19,17 +27,17 @@ export default {
     return {
       advantages: [
         {
-          img: require('@/assets/img/icons/iconOne.png'),
+          img: require('@/assets/img/icons/fastTransactions.svg'),
           title: "Very fast 5-10 min transactions",
           text: "Swapuz provides the best exchange rate and transparent fee structure with no hidden fee."
         },
         {
-          img: require('@/assets/img/icons/iconTwo.png'),
+          img: require('@/assets/img/icons/exchangeLimits.svg'),
           title: "High exchange limits",
           text: "Swapuz provides the best exchange rate and transparent fee structure with no hidden fee."
         },
         {
-          img: require('@/assets/img/icons/iconThree.png'),
+          img: require('@/assets/img/icons/reasonableFees.svg'),
           title: "Transparent reasonable fees",
           text: "Swapuz provides the best exchange rate and transparent fee structure with no hidden fee."
         },
