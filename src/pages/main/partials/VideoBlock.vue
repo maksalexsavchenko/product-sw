811e4afdb1e8bef734b43b80eea0609447a829e5
Dog <dziubaviacheslav@.icloud.com>
2022-10-13 17:02:48 +0300

    features

--- src/pages/main/partials/VideoBlock.vue
+++ src/pages/main/partials/VideoBlock.vue
@@ -1,18 +1,24 @@
 <template>
 <div class="home-video">
-  <div class="app-section mobile-hide">
+  <div class="app-section">
     <div class="container">
       <div class="app-section-text">
+        <div class="app-section-text-updated">
+          Updated
+        </div>
         <div class="app-section-text-header">
           Install all new <br>
-          Swapuz app
+          Swapuz <b>app</b>
         </div>
         <div class="app-section-text-desc">
           Safely, anonymously and fast crypto swaps with 24/7 support
         </div>
         <div class="app-section-text-links">
           <a href="https://play.google.com/store/apps/details?id=com.swapuz.mobile" target="_blank">
-            <img src="../../../assets/googlelink.svg" alt="">
+            <google-link />
+          </a>
+          <a href="#" target="_blank">
+            <apple-link />
           </a>
         </div>
       </div>
@@ -31,39 +37,47 @@
       </div>
     </div>
   </div>
-  <div class="app-section-mob mobile-show">
+  <div class="app-section-mob">
     <div class="app-section-mob-block">
       <div class="content">
-        <div class="text">
-          <div class="header">
-            Install all new <br>
-            Swapuz app
-          </div>
-          <div class="desc">
-            Safely, anonymously and fast crypto swaps with 24/7 support
-          </div>
+        <div class="content-updated">
+          Update
         </div>
-        <div class="links">
+        <div class="content-header">
+          Install all new <br>
+          Swapuz <b>app</b>
+        </div>
+        <div class="content-desc">
+          Safely, anonymously and fast crypto swaps with 24/7 support
+        </div>
+        <div class="content-links">
           <a href="https://play.google.com/store/apps/details?id=com.swapuz.mobile" target="_blank">
-            <img src="../../../assets/googlemob.svg" alt="">
+            <google-mob />
+          </a>
+          <a class="space" href="#" target="_blank">
+            <apple-mob />
           </a>
         </div>
       </div>
       <div class="bg">
         <img
-            srcset="../../../assets/banner/bannerbg2x.png 1x,
-             ../../../assets/banner/bannerbg3x.png 2x"
-            src="../../../assets/banner/bannerbg2x.png">
+          srcset="../../../assets/banner/bannerbg2x.png 1x,
+          ../../../assets/banner/bannerbg3x.png 2x"
+          src="../../../assets/banner/bannerbg2x.png">
       </div>
-
     </div>
   </div>
 </div>
 </template>
 
 <script>
+import GoogleLink from "@/assets/GoogleLink.vue";
+import AppleLink from "@/assets/AppleLink.vue";
+import GoogleMob from "@/assets/GoogleMob.vue";
+import AppleMob from "@/assets/AppleMob.vue";
 export default {
   name: "VideoBlock",
+  components: { GoogleLink, AppleLink, GoogleMob, AppleMob },
   data() {
     return {
       isMobDev: false,
@@ -98,150 +112,5 @@ export default {
 </script>
 
 <style lang="scss">
-.app-section {
-  background: linear-gradient(180deg, rgba(255, 255, 255, 0.03) 0%, rgba(255, 255, 255, 0.02) 100%);
-  min-height: 560px;
-  margin: 120px 0;
-  overflow: hidden;
-
-  .container {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-  }
-
-  &-phone {
-    position: relative;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    &-bg {
-      position: absolute;
-      width: 772px;
-      height: 600px;
-      top: 0;
-      left: 0;
-      z-index: -1;
-    }
-
-    &-block {
-      display: flex;
-      justify-content: center;
-      align-items: flex-start;
-
-      .phone {
-        position: relative;
-        z-index: 1;
-        max-width: 375.34165px;
-        transform: translateX(85px);
-      }
-    }
-  }
-
-  &-text {
-    margin: 80px 0 102px;
-
-    &-header {
-      font-weight: 900;
-      font-size: 64px;
-      line-height: 70px;
-      color: #FFFFFF;
-      margin-bottom: 24px;
-    }
-
-    &-desc {
-      font-weight: 300;
-      font-size: 24px;
-      line-height: 32px;
-      color: #EEEDEE;
-      opacity: 0.7;
-      margin-bottom: 86px;
-      max-width: 400px;
-    }
-
-    &-links {
-      display: flex;
-      flex-direction: row;
-      width: 450px;
-      justify-content: space-between;
-    }
-  }
-
-  &-mob {
-    padding: 0 4%;
-    margin-top: 56px;
-    margin-bottom: 50px;
-
-    &-block {
-      //border-radius: 24px;
-      //background: linear-gradient(121.01deg, rgba(255, 255, 255, 0.05) 0.89%, rgba(255, 255, 255, 0) 81.06%), #1B1A1C;
-      //border: 1px solid rgba(255, 255, 255, 0.1);
-      position: relative;
-      min-height: 680px;
-
-      .content {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        position: relative;
-        z-index: 9;
-
-        .text {
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          margin-bottom: 28px;
-
-          .header {
-            font-weight: 900;
-            font-size: 44px;
-            line-height: 44px;
-            text-align: center;
-            color: #FFFFFF;
-            margin-bottom: 20px;
-            margin-top: 24px;
-          }
-
-          .desc {
-            font-weight: 300;
-            font-size: 14px;
-            line-height: 20px;
-            text-align: center;
-            color: #EEEDEE;
-            max-width: 240px;
-          }
-        }
-        .links {
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-
-          a:first-child {
-            margin-bottom: 12px;
-          }
-        }
-      }
-
-      .bg {
-        width: 100%;
-        z-index: 1;
-
-        img {
-          max-width: 100%;
-          width: 100%;
-          border-radius: 24px;
-          height: 100%;
-          position: absolute;
-          bottom: 0;
-
-        }
-      }
-    }
-  }
-}
+@import "../../../assets/styles/pages/main/video-block";
 </style>
